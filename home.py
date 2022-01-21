efdf1c2fd1ac37c5b72ea41894b0b0b91753a552
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2022-01-21 03:16:33 +0200

    equation entry

--- home.py
+++ home.py
@@ -12,28 +12,42 @@ class RootCalculator:
         # super().__init__(master)
         # self.self=master
         self.master = master
-        self.myFrame = ttk.LabelFrame(self.master, text="Linear Equations")
-        self.myFrame.pack(fill='both', side='left', anchor="nw", expand='false', padx=10, pady=10)
+        # self.myFrame = ttk.LabelFrame(self.master, text="Linear Equations")
+        # self.myFrame.pack(fill='both', side='left', anchor="nw", expand='false', padx=10, pady=10)
         self.methodFrame = ttk.LabelFrame(self.master, text="Choose Solving Method")
-        self.methodFrame.pack(fill='both', side='top', expand='false', pady=10)
-        self.tableFrame = ttk.LabelFrame(self.master, text="Table")
-        self.tableFrame.pack(fill='both', side='bottom', anchor="sw", expand='true')
+        self.methodFrame.pack(fill='both', side='top', expand='false', padx=10, pady=10)
+        self.tableFrame = ttk.LabelFrame(self.master, text="Systems of Linear Equations")
+        self.tableFrame.pack(fill='both', side='bottom', anchor="sw", expand='true', padx=10, pady=10)
         # self.iterationsTable = ttk.Treeview(self.tableFrame)
         # self.resultLabel = ttk.Label(self.tableFrame)
         # self.executionTimeLabel = ttk.Label(self.tableFrame)
         # self.errorLabel = ttk.Label(self.tableFrame)
-        self.createButton()
+        self.entries = []
+        self.labels = []
+        self.equalLabels = []
+        self.equalEntries = []
+        self.solveButton = ttk.Button(self.tableFrame, text="Solve")
+        # self.createButton(2)
         self.createMethodSelector()
 
     def createMethodSelector(self):
+        self.degreeLabel = ttk.Label(self.methodFrame, text="Degree")
+        self.degreeLabel.grid(row=0, column=0)
+        self.degreeOptions = ["1", "2", "3", "4", "5"]
+        self.degreeClicked = tk.StringVar()
+        self.degreeClicked.set(self.degreeOptions[0])
+        self.degreeSelect = tk.OptionMenu(self.methodFrame, self.degreeClicked, *self.degreeOptions)
+        self.degreeSelect.grid(row=0, column=1, padx=10, pady=5)
+        self.degreeClicked.trace("w", self.changeNumberOfEquations)
+
         self.methodLabel = ttk.Label(self.methodFrame, text="Method")
-        self.methodLabel.grid(row=0, column=0)
-        self.options = ["Gaussian-elimination", "LU decomposition,", "Gaussian-Jordan", "Gauss-Seidel"]
+        self.methodLabel.grid(row=0, column=2)
+        self.options = ["Gaussian-elimination", "LU decomposition", "Gaussian-Jordan", "Gauss-Seidel"]
         self.clicked = tk.StringVar()
         self.clicked.set(self.options[0])
         # self.clicked.trace("w", self.changeEntry)
         self.methodSelect = tk.OptionMenu(self.methodFrame, self.clicked, *self.options)
-        self.methodSelect.grid(row=0, column=1, padx=10, pady=5)
+        self.methodSelect.grid(row=0, column=3, padx=10, pady=5)
         self.lowerBoundLabel = ttk.Label(self.methodFrame, text="x (lower bound) ")
         self.lowerBoundLabel.grid(row=1, column=0, padx=10, pady=3)
         self.lowerBoundEntry = ttk.Entry(self.methodFrame)
@@ -57,6 +71,43 @@ class RootCalculator:
         self.singleMode = tk.IntVar()
         single_mode_check_box = ttk.Checkbutton(self.methodFrame, text="Single Mode", variable=self.singleMode)
         single_mode_check_box.grid(row=2, column=6, padx=40, pady=20)
+
+    def changeNumberOfEquations(self, *args):
+        for entry in self.entries:
+            entry.grid_forget()
+        for label in self.labels:
+            label.grid_forget()
+        for equalLabel in self.equalLabels:
+            equalLabel.grid_forget()
+        for equalEntry in self.equalEntries:
+            equalEntry.grid_forget()
+        self.entries.clear()
+        self.labels.clear()
+        self.equalLabels.clear()
+        self.equalEntries.clear()
+        self.solveButton.grid_forget()
+
+        for i in range(int(self.degreeClicked.get())):
+            flag = 0
+            for j in range(2 * int(self.degreeClicked.get())):
+                if flag == 0:
+                    self.xLabel = ttk.Label(self.tableFrame, text="x{}".format(j // 2 + 1))
+                    self.xLabel.grid(row=i, column=j, padx=5, pady=5)
+                    self.labels.append(self.xLabel)
+                    flag = 1
+                else:
+                    self.xEntry = ttk.Entry(self.tableFrame, width=10)
+                    self.xEntry.grid(row=i, column=j, padx=5, pady=5)
+                    self.entries.append(self.xEntry)
+                    flag = 0
+            self.equalLabel = ttk.Label(self.tableFrame, text="=")
+            self.equalLabel.grid(row=i, column=j + 1, padx=5, pady=5)
+            self.equalLabels.append(self.equalLabel)
+            self.equalEntry = ttk.Entry(self.tableFrame, width=10)
+            self.equalEntry.grid(row=i, column=j + 2, padx=5, pady=5)
+            self.equalEntries.append(self.equalEntry)
+
+        self.solveButton.grid(row=i + 1, column=0, columnspan=20, padx=5, pady=5, sticky=tk.W + tk.E)
 
     def changeEntry(self, *args):
         if self.clicked.get() == "Fixed Point" or self.clicked.get() == "Newton Raphson":
@@ -105,102 +156,6 @@ class RootCalculator:
             self.functionEntry.insert(0, "Error Reading File")
         file.close()
 
-    def createButton(self):
-        self.functionEntry1 = ttk.Entry(self.myFrame, width=50)
-        self.functionEntry1.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-        self.functionEntry2 = ttk.Entry(self.myFrame, width=50)
-        self.functionEntry2.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-        self.functionEntry3 = ttk.Entry(self.myFrame, width=50)
-        self.functionEntry3.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
-
-        self.functionEntry1.event_generate('<Control-v>')
-        self.functionEntry1.event_generate('<Control-x>')
-        self.functionEntry1.event_generate('<Control-c>')
-        self.functionEntry2.event_generate('<Control-v>')
-        self.functionEntry2.event_generate('<Control-x>')
-        self.functionEntry2.event_generate('<Control-c>')
-        self.functionEntry3.event_generate('<Control-v>')
-        self.functionEntry3.event_generate('<Control-x>')
-        self.functionEntry3.event_generate('<Control-c>')
-
-        self.button0 = ttk.Button(self.myFrame, text="0", command=lambda: self.buttonClick(0))
-        self.button1 = ttk.Button(self.myFrame, text="1", command=lambda: self.buttonClick(1))
-        self.button2 = ttk.Button(self.myFrame, text="2", command=lambda: self.buttonClick(2))
-        self.button3 = ttk.Button(self.myFrame, text="3", command=lambda: self.buttonClick(3))
-        self.button4 = ttk.Button(self.myFrame, text="4", command=lambda: self.buttonClick(4))
-        self.button5 = ttk.Button(self.myFrame, text="5", command=lambda: self.buttonClick(5))
-        self.button6 = ttk.Button(self.myFrame, text="6", command=lambda: self.buttonClick(6))
-        self.button7 = ttk.Button(self.myFrame, text="7", command=lambda: self.buttonClick(7))
-        self.button8 = ttk.Button(self.myFrame, text="8", command=lambda: self.buttonClick(8))
-        self.button9 = ttk.Button(self.myFrame, text="9", command=lambda: self.buttonClick(9))
-
-        # self.buttonExp = ttk.Button(self.myFrame, text="exp", command=lambda: self.buttonClick("e("))
-        # self.buttonTan = ttk.Button(self.myFrame, text="tan", command=lambda: self.buttonClick("tan("))
-        # self.buttonSin = ttk.Button(self.myFrame, text="sin", command=lambda: self.buttonClick("sin("))
-        # self.buttonCos = ttk.Button(self.myFrame, text="cos", command=lambda: self.buttonClick("cos("))
-        # self.buttonLn = ttk.Button(self.myFrame, text="ln", command=lambda: self.buttonClick("ln("))
-        self.buttonX = ttk.Button(self.myFrame, text="x", command=lambda: self.buttonClick("x"))
-        self.buttonPoint = ttk.Button(self.myFrame, text=".", command=lambda: self.buttonClick("."))
-
-
-        self.buttonPlus = ttk.Button(self.myFrame, text="+", command=lambda: self.buttonClick("+"))
-        self.buttonMult = ttk.Button(self.myFrame, text="*", command=lambda: self.buttonClick("*"))
-        # self.buttonDiv = ttk.Button(self.myFrame, text="/", command=lambda: self.buttonClick("/"))
-        self.buttonMinus = ttk.Button(self.myFrame, text="-", command=lambda: self.buttonClick("-"))
-        # self.buttonPower = ttk.Button(self.myFrame, text="^", command=lambda: self.buttonClick("^("))
-        # self.buttonBracketLeft = ttk.Button(self.myFrame, text="(", command=lambda: self.buttonClick("("))
-        # self.buttonBracketRight = ttk.Button(self.myFrame, text=")", command=lambda: self.buttonClick(")"))
-
-        self.buttonDel = ttk.Button(self.myFrame, text="Del", command=lambda: self.buttonDelete())
-        self.buttonAC1 = ttk.Button(self.myFrame, text="AC", command=lambda: self.buttonClearClick())
-        self.buttonAC2 = ttk.Button(self.myFrame, text="AC", command=lambda: self.buttonClearClick())
-        self.buttonAC3 = ttk.Button(self.myFrame, text="AC", command=lambda: self.buttonClearClick())
-
-        self.buttonEqual = ttk.Button(self.myFrame, text="Solve", command=lambda: self.buttonCalculate())
-
-        self.buttonAC1.grid(row=0, column=3, padx=5, pady=5)
-        self.buttonAC2.grid(row=1, column=3, padx=5, pady=5)
-        self.buttonAC3.grid(row=2, column=3, padx=5, pady=5)
-        self.buttonDel.grid(row=3, column=3, padx=5, pady=5)
-
-        self.button7.grid(row=3, column=0, padx=5, pady=5)
-        self.button8.grid(row=3, column=1, padx=5, pady=5)
-        self.button9.grid(row=3, column=2, padx=5, pady=5)
-        self.button4.grid(row=4, column=0, padx=5, pady=5)
-        self.button5.grid(row=4, column=1, padx=5, pady=5)
-        self.button6.grid(row=4, column=2, padx=5, pady=5)
-        self.button1.grid(row=5, column=0, padx=5, pady=5)
-        self.button2.grid(row=5, column=1, padx=5, pady=5)
-        self.button3.grid(row=5, column=2, padx=5, pady=5)
-        self.button0.grid(row=6, column=1, padx=5, pady=5)
-
-        self.buttonMult.grid(row=4, column=3, padx=5, pady=5)
-        self.buttonMinus.grid(row=5, column=3, padx=5, pady=5)
-        self.buttonPlus.grid(row=6, column=3, padx=5, pady=5)
-        self.buttonX.grid(row=6, column=2, padx=5, pady=5)
-        self.buttonPoint.grid(row=6, column=0, padx=5, pady=5)
-        self.buttonEqual.grid(row=7, column=0, columnspan=4, rowspan=2, padx=5, pady=5, sticky=tk.W + tk.E)
-
-        self.readmeString = "Entry Restrictions:\n" \
-                            "\tIn The Form: 3*a + 2*b + c -6\n" \
-                            "\tMultiplication 4*x not 4x\n"
-        self.readmeLabel = ttk.Label(self.myFrame, text=self.readmeString)
-        self.readmeLabel.grid(row=9, column=0, columnspan=4, padx=10, pady=40)
-
-    def buttonClick(self, op):
-        self.myFrame.focus_get()
-        # self.functionEntry.insert(tk.END, op)
-        # self.functionEntry1.index(tk.INSERT)
-
-    def buttonClearClick(self):
-        self.functionEntry.delete(0, tk.END)
-
-    def buttonDelete(self):
-        self.input = self.functionEntry.get()
-        self.input = self.input[:-1]
-        self.functionEntry.delete(0, tk.END)
-        self.functionEntry.insert(0, self.input)
-
     def buttonCalculate(self):
         # plt.close("all")
         # self.errorLabel['text'] = ""
@@ -243,18 +198,6 @@ class RootCalculator:
         #     self.iterationsTable.pack_forget()
         #     self.errorLabel.pack(padx=20, pady=(20, 0))
 
-    # def plotFunction(self):
-    #     x_list = np.linspace(-10, 10, num=1000)
-    #     plt.figure(num=0, dpi=120)
-    #     plt.plot(x_list, self.func(x_list))
-    #     plt.title("f(x)")
-    #     plt.xlabel("x")
-    #     plt.grid()
-    #     plt.ylabel("y")
-    #     plt.xlim(-10, 10)
-    #     plt.ylim(-10, 10)
-    #     plt.show()
-
     # def func(self, x):
     #     expression = self.input.replace("^", "**")
     #     expression = expression.replace("cos", "np.cos")
@@ -263,76 +206,6 @@ class RootCalculator:
     #     expression = expression.replace("ln", "np.log")
     #     expression = expression.replace("exp", "np.exp")
     #     return eval(expression)
-
-    # def createTable(self, result):
-    #     data = result['table']
-    #     self.executionTimeLabel['text'] = ""
-    #     self.resultLabel['text'] = ""
-    #     for item in self.iterationsTable.get_children():
-    #         self.iterationsTable.delete(item)
-    #     self.iterationsTable['columns'] = ("i", "Xl", "Xu", "Xr", "f(Xr)", "Relative Error")
-    #     self.iterationsTable.column("#0", width=0, stretch=tk.NO)
-    #     self.iterationsTable.column("i", anchor=tk.W, width=30)
-    #     self.iterationsTable.column("Xl", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("Xu", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("Xr", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("f(Xr)", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("Relative Error", anchor=tk.W, width=150)
-    #
-    #     self.iterationsTable.heading("i", text="i", anchor=tk.W)
-    #     self.iterationsTable.heading("Xl", text="Xl", anchor=tk.W)
-    #     self.iterationsTable.heading("Xu", text="Xu", anchor=tk.W)
-    #     self.iterationsTable.heading("Xr", text="Xr", anchor=tk.W)
-    #     self.iterationsTable.heading("f(Xr)", text="f(Xr)", anchor=tk.W)
-    #     self.iterationsTable.heading("Relative Error", text="Relative Error", anchor=tk.W)
-    #
-    #     count = 0
-    #     for record in data:
-    #         self.iterationsTable.insert(parent="", index="end", iid=count, text="",
-    #                                     values=(record[0], record[1], record[2], record[3], record[4], record[5]))
-    #         count += 1
-    #     self.iterationsTable.pack(padx=20, pady=20)
-    #     self.resultString = "Root x at i=" + str(count) + ":\t" + str(data[-1][3])
-    #     self.resultLabel['text'] = self.resultString
-    #     self.resultLabel.pack(padx=20, pady=10)
-    #     self.executionTimeString = "Execution Time = " + str(result["execution_time"]) + "s"
-    #     self.executionTimeLabel['text'] = self.executionTimeString
-    #     self.executionTimeLabel.pack(padx=20, pady=10)
-    #
-    # def newtonRaphsonTable(self, result):
-    #     data = result['table']
-    #     self.executionTimeLabel['text'] = ""
-    #     self.resultLabel['text'] = ""
-    #     for item in self.iterationsTable.get_children():
-    #         self.iterationsTable.delete(item)
-    #     self.iterationsTable['columns'] = ("i", "Xold", "Xnew", "f(x)", "f`(x)", "Relative Error")
-    #     self.iterationsTable.column("#0", width=0, stretch=tk.NO)
-    #     self.iterationsTable.column("i", anchor=tk.W, width=30)
-    #     self.iterationsTable.column("Xold", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("Xnew", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("f(x)", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("f`(x)", anchor=tk.W, width=150)
-    #     self.iterationsTable.column("Relative Error", anchor=tk.W, width=150)
-    #
-    #     self.iterationsTable.heading("i", text="i", anchor=tk.W)
-    #     self.iterationsTable.heading("Xold", text="Xold", anchor=tk.W)
-    #     self.iterationsTable.heading("Xnew", text="Xnew", anchor=tk.W)
-    #     self.iterationsTable.heading("f(x)", text="f(x)", anchor=tk.W)
-    #     self.iterationsTable.heading("f`(x)", text="f`(x)", anchor=tk.W)
-    #     self.iterationsTable.heading("Relative Error", text="Relative Error", anchor=tk.W)
-    #
-    #     count = 0
-    #     for record in data:
-    #         self.iterationsTable.insert(parent="", index="end", iid=count, text="",
-    #                                     values=(record[0], record[1], record[2], record[3], record[4], record[5]))
-    #         count += 1
-    #     self.iterationsTable.pack(padx=20, pady=20)
-    #     self.resultString = "Root x at i=" + str(count) + ":\t" + str(data[-1][2])
-    #     self.resultLabel['text'] = self.resultString
-    #     self.resultLabel.pack(padx=20, pady=10)
-    #     self.executionTimeString = "Execution Time = " + str(result["execution_time"]) + "s"
-    #     self.executionTimeLabel['text'] = self.executionTimeString
-    #     self.executionTimeLabel.pack(padx=20, pady=10)
 
 
 root = tk.Tk()
